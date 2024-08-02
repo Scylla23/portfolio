b44c82b59073cd6b86389fdfd3bc6e4c19091248
Pavan Kushnure <pavankushnure@Pavans-MacBook-Air.local>
2024-08-02 19:34:05 +0530

    changed copy

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ const App: React.FC = () => {
             >
               <img
                 src="/me.gif"
-                alt="Biswarup Sen"
+                alt="Pavan Kushnure"
                 className="w-full h-full object-cover"
               />
             </motion.div>
